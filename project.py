4f08183942a7e4703093a064ff27b2f51d1d4815
ManI'mDead <cte-elijah.snyder@sllboces.org>
2026-04-22 14:54:16 +0000

    Save 1

---
+++ project.py
@@ -0,0 +1,30 @@
+"""
+Game: A Squires Tale - 
+A text-based Adventure Game about a small Squire (You) on an adventure to knighthood, will you become a knight, or will you fail trying?
+"""
+
+class MainCharacter:
+    def __init__(self):
+        self.name = name
+        self.age = age
+        self.species = species
+    def getStats(self):
+        if self.species == "Human" or self.species == "human":
+            strength = 10
+            dexterity = 10
+            intelligence = 10
+            endurance = 10
+            charisma = 10
+            luck = 10
+            mp = 10
+        elif self.species == "Elf" or self.species == "elf":
+            strength = 10
+            dexterity = 10
+            intelligence = 10
+            endurance = 5
+            charisma = 10
+            luck = 10
+            mp = 20
+
+
+    
