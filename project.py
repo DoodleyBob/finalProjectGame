a5b671d1405a30283fe3e60296320b142893676f
ManI'mDead <cte-elijah.snyder@sllboces.org>
2026-05-11 14:48:13 +0000

    Save 4

--- project.py
+++ project.py
@@ -2,81 +2,82 @@
 Game: A Squires Tale - 
 A text-based Adventure Game about a small Squire (You) on an adventure to knighthood, will you become a knight, or will you fail trying?
 """
-allowed_species = ["Human", "human", "Elf", "elf", "Half-Elf", "Half-elf", "half-elf", "Dwarf", "dwarf", "Teifling", "teifling", "Halfling", "halfling", "Dragonborn", "dragonborn", "Half-Orc", "Half-orc", "half-orc"]
+allowed_races = ["Human", "human", "Elf", "elf", "Half-Elf", "Half-elf", "half-elf", "Dwarf", "dwarf", "Teifling", "teifling", "halfling", "dragonborn", "half-orc"]
 
 class MainCharacter:
-    def __init__(self):
+    def __init__(self, name, race):
         self.name = name
-        self.age = age
-        self.species = species
+        self.race = race
     def getStats(self):
-        while self.species not in allowed_species:
-            self.species = input("Enter your race - (Human, Elf, Half-Elf, Dwarf, Teifling, Halfling, Dragonborn, Half-Orc): ")
-            if self.species == "Human" or self.species == "human":
-                strength = 10
-                dexterity = 10
-                intelligence = 10
-                endurance = 10
-                charisma = 12
-                luck = 12
-                mp = 10
-            elif self.species == "Elf" or self.species == "elf":
-                strength = 10
-                dexterity = 10
-                intelligence = 12
-                endurance = 10
-                charisma = 10
-                luck = 10
-                mp = 12
-            elif self.species == "Half-Elf" or self.species == "Half-elf" or self.species == "half-elf":
-                strength = 10
-                dexterity = 10
-                intelligence = 10
-                endurance = 10
-                charisma = 12
-                luck = 10
-                mp = 12
-            elif self.species == "Dwarf" or self.species == "dwarf":
-                strength = 12
-                dexterity = 10
-                intelligence = 10
-                endurance = 12
-                charisma = 10
-                luck = 10
-                mp = 10
-            elif self.species == "Teifling" or self.species == "teifling":
-                strength = 10
-                dexterity = 12
-                intelligence = 12
-                endurance = 10
-                charisma = 10
-                luck = 10
-                mp = 10
-            elif self.species == "Halfling" or self.species == "halfling":
-                strength = 10    
-                dexterity = 12
-                intelligence = 11
-                endurance = 10
-                charisma = 10
-                luck = 11
-                mp = 10
-            elif self.species == "Dragonborn" or self.species == "dragonborn":
-                strength = 12
-                dexterity = 10
-                intelligence = 10
-                endurance = 10
-                charisma = 12
-                luck = 10
-                mp = 10
-            elif self.species == "Half-Orc" or self.species == "Half-orc" or self.species == "half-orc":
-                strength = 12
-                dexterity = 10
-                intelligence = 10
-                endurance = 11
-                charisma = 10
-                luck = 10
-                mp = 11
-            else:
-                self.species = input("That race does not exist, please re-input a race out of the races provided - (Human, Elf, Half-Elf, Dwarf, Teifling, Halfling, Dragonborn, Half-Orc): ")
+        while self.race not in allowed_races:
+            self.race = input("Please Enter your race - (Human, Elf, Half-Elf, Dwarf, Teifling, Halfling, Dragonborn, Half-Orc): ").lower()
+        if self.race == "human":
+            self.strength = 10
+            self.dexterity = 10
+            self.intelligence = 10
+            self.endurance = 10
+            self.charisma = 12
+            self.luck = 12
+            self.mp = 10
+        elif self.race == "elf":
+            self.strength = 10
+            self.dexterity = 10
+            self.intelligence = 12
+            self.endurance = 10
+            self.charisma = 10
+            self.luck = 10
+            self.mp = 12
+        elif self.race == "half-elf":
+            self.strength = 10
+            self.dexterity = 10
+            self.intelligence = 10
+            self.endurance = 10
+            self.charisma = 12
+            self.luck = 10
+            self.mp = 12
+        elif self.race == "dwarf":
+            self.strength = 12
+            self.dexterity = 10
+            self.intelligence = 10
+            self.endurance = 12
+            self.charisma = 10
+            self.luck = 10
+            self.mp = 10
+        elif self.race == "teifling":
+            self.strength = 10
+            self.dexterity = 12
+            self.intelligence = 12
+            self.endurance = 10
+            self.charisma = 10
+            self.luck = 10
+            self.mp = 10
+        elif self.race == "halfling":
+            self.strength = 10    
+            self.dexterity = 12
+            self.intelligence = 11
+            self.endurance = 10
+            self.charisma = 10
+            self.luck = 11
+            self.mp = 10
+        elif self.race == "dragonborn":
+            self.strength = 12
+            self.dexterity = 10
+            self.intelligence = 10
+            self.endurance = 10
+            self.charisma = 12
+            self.luck = 10
+            self.mp = 10
+        elif self.race == "half-orc":
+            self.strength = 12
+            self.dexterity = 10
+            self.intelligence = 10
+            self.endurance = 11
+            self.charisma = 10
+            self.luck = 10
+            self.mp = 11
 
-
+name = input("Please enter your name: ")
+race = input("What is your race? (Human, Elf, Half-Elf, Dwarf, Teifling, Halfling, Dragonborn, Half-Orc): ")
+player = MainCharacter(name, race)
+player.getStats()
+print(f"I am {player.name} the {player.race}, and have {player.mp} mp.")
